702cbf03ba9798814661953d336330d1aecba269
hopechings <hopechings@hopechingsui-MacBookPro.local>
2024-02-27 16:37:04 +0900

    init

---
+++ GitAdaption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitAdaption
+//
+//  Created by hopechings on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
