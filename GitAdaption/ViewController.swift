a7b92383cf7a869206a50f58dedbe785b56a0c86
hopechings <hopechings@hopechingsui-MacBookPro.local>
2024-02-27 16:42:32 +0900

    modify

--- GitAdaption/ViewController.swift
+++ GitAdaption/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let vm: ViewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        vm.setup()
     }
-
 
 }
 
+class ViewModel {
+    func setup() {
+        print("setup")
+    }
+}
